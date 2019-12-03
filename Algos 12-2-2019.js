4b821e759f25590befb8940ba99ee2d597f26e46
Ken S <gxcadr@gmail.com>
2019-12-03 00:46:06 -0800

    initial solution for maxBaloons

--- Algos 12-2-2019.js
+++ Algos 12-2-2019.js
@@ -1,33 +1,45 @@
 /**
- * @param {string} A
- * @param {string} B
- * @return {string[]}
+ * @param {string} text
+ * @return {number}
  */
-const uncommonFromSentences = function(A, B) {
-  const uncommonWords = [];
-  const wordsA = A.split(' ');
-  const wordsB = B.split(' ');
+const maxNumberOfBalloons = function(text) {
+  let count = 0;
+  const letterCount = {};
 
-  const singleOccuranceTracker = {};
-  const multipleOccuranceTracker = {};
+  for (let i = 0; i < text.length; i += 1) {
+    letterCount[text[i]] = (letterCount[text[i]] || 0) + 1;
+  }
 
-  wordsA.forEach((word, index, array) => {
-    if (singleOccuranceTracker[word] === undefined && multipleOccuranceTracker[word] === undefined) {
-      singleOccuranceTracker[word] = true;
-    } else if (singleOccuranceTracker[word] !== undefined) {
-      multipleOccuranceTracker[word] = true;
-      delete singleOccuranceTracker[word];
+  while (letterCount.b > 0) {
+    if (letterCount.b >= 1) {
+      letterCount.b -= 1;
+    } else {
+      break;
     }
-  });
-
-  wordsB.forEach((word, index, array) => {
-    if (singleOccuranceTracker[word] === undefined && multipleOccuranceTracker[word] === undefined) {
-      singleOccuranceTracker[word] = true;
-    } else if (singleOccuranceTracker[word] !== undefined) {
-      multipleOccuranceTracker[word] = true;
-      delete singleOccuranceTracker[word];
+    if (letterCount.a >= 1) {
+      letterCount.a -= 1;
+    } else {
+      break;
     }
-  });
+    if (letterCount.l >= 2) {
+      letterCount.l -= 2;
+    } else {
+      break;
+    }
+    if (letterCount.o >= 2) {
+      letterCount.o -= 2;
+    } else {
+      break;
+    }
+    if (letterCount.n >= 1) {
+      letterCount.n -= 1;
+    } else {
+      break;
+    }
+    count += 1;
+  }
 
-  return Object.keys(singleOccuranceTracker);
+  return count;
 };
+
+console.log(maxNumberOfBalloons('loonbalxballpoon')); // --> 2
