f545fc428c112db3b02fa0845d902d4f0bc05e31
Ken S <gxcadr@gmail.com>
2019-12-02 23:52:26 -0800

    initial commit for algos 12-2-2019

--- Algos 12-2-2019.js
+++ Algos 12-2-2019.js
@@ -1,49 +1,4 @@
 /**
- * Initialize your data structure here.
- */
-const MyHashSet = function() {
-  this.hashmap = {};
-};
-
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.add = function(key) {
-  this.hashmap[key] = true;
-};
-
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function(key) {
-  delete this.hashmap[key];
-};
-
-/**
- * Returns true if this set contains the specified element 
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function(key) {
-  if (this.hashmap[key] !== undefined) {
-    return true;
-  }
-  return false;
-};
-
-
-//  Your MyHashSet object will be instantiated and called as such:
-const key = 5;
- const obj = new MyHashSet();
- obj.add(key);
- obj.remove(key);
- let param_3 = obj.contains(key);
-
-//  console.log(param_3);
-
-/**
  * @param {string} A
  * @param {string} B
  * @return {string[]}
@@ -76,8 +31,3 @@ const uncommonFromSentences = function(A, B) {
 
   return Object.keys(singleOccuranceTracker);
 };
-
-const A = 'this apple is sweet';
-const B = 'this apple is sour';
-
-console.log(uncommonFromSentences(A, B));
