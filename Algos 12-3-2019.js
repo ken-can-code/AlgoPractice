a53c60b5ea3c3fd3af1056acfd9908f29a5b6912
Ken S <gxcadr@gmail.com>
2019-12-03 23:55:12 -0800

    slightly improved solution for rangeSumBST

--- Algos 12-3-2019.js
+++ Algos 12-3-2019.js
@@ -33,8 +33,12 @@ const rangeSumBST = function(root, L, R) {
     sum += root.val;
   }
 
-  sum += rangeSumBST(root.left, L, R);
-  sum += rangeSumBST(root.right, L, R);
+  if (root.val >= L) {
+    sum += rangeSumBST(root.left, L, R);
+  }
+  if (root.val <= R) {
+    sum += rangeSumBST(root.right, L, R);
+  }
 
   return sum;
 };
