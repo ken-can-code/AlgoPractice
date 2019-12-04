f824e4089b6a4fbeb0ca013cd74633ce6ec106db
Ken S <gxcadr@gmail.com>
2019-12-03 23:18:31 -0800

    solution for binary tree larger sum branch

---
+++ Algos 12-3-2019.js
@@ -0,0 +1,69 @@
+const myMathMax = (numbers) => {
+  let max = -Infinity;
+
+  numbers.forEach((num) => {
+    if (num > max) {
+      max = num;
+    }
+  });
+
+  return max;
+};
+
+console.log(myMathMax([7, 3, 4, 5]));
+
+const binaryTreeLargerSumBranch = (arr) => {
+  if (arr.length < 1) {
+    return '';
+  }
+  let leftSide = [];
+  let rightSide = [];
+  let sliceSize = 1;
+  let returnString = '';
+  let i = 1;
+
+  while (i < arr.length) {
+    leftSide = leftSide.concat(arr.slice(i, i + sliceSize));
+    sliceSize *= 2;
+    i += sliceSize;
+  }
+  i = 2;
+  sliceSize = 1;
+  let incrementer = 3;
+
+  while (i < arr.length) {
+    rightSide = rightSide.concat(arr.slice(i, i + sliceSize));
+    sliceSize *= 2;
+    i += incrementer;
+    incrementer *= 2;
+  }
+
+  const leftTotal = leftSide.reduce((acc, num) => {
+    if (num > 0) {
+      return acc + num;
+    }
+    return acc;
+  });
+  const rightTotal = rightSide.reduce((acc, num) => {
+    if (num > 0) {
+      return acc + num;
+    }
+    return acc;
+  });
+
+  if (leftTotal > rightTotal) {
+    returnString = 'Left';
+  } else if (rightTotal > leftTotal) {
+    returnString = 'Right';
+  }
+
+  return returnString;
+};
+
+const testArray = [];
+
+for (let i = 1; i < 35; i += 1) {
+  testArray.push(i);
+}
+
+console.log(binaryTreeLargerSumBranch([3, 5, 7, 10, -1, 9]));
